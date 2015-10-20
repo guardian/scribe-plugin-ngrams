5d6b1c14ee28ef3b59fe260c1f8a67ad37738e38
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-10-20 15:13:27 +0100

    Update the way the words are replaced and the way the replacer works

--- src/scribe-plugin-ngrams.js
+++ src/scribe-plugin-ngrams.js
@@ -5,34 +5,24 @@ module.exports = function(config) {
     const NGrams = natural.NGrams;
 
     return function(scribe) {
-        let unWords = [
-            (text) => text.replace(/ the /g, " "),
-            (text) => text.replace(/ and /g, " "),
-            (text) => text.replace(/ but /g, " "),
-            (text) => text.replace(/ then /g, " "),
-            (text) => text.replace(/ a /g, " "),
-            (text) => text.replace(/ of /g, " "),
-            (text) => text.replace(/ an /g, " "),
-            (text) => text.replace(/ in /g, " "),
-            (text) => text.replace(/ on /g, " "),
-            (text) => text.replace(/ by /g, " "),
-            (text) => text.replace(/ he /g, " "),
-            (text) => text.replace(/ she /g, " "),
-            (text) => text.replace(/ had /g, " "),
-            (text) => text.replace(/ gone /g, " "),
-            (text) => text.replace(/ that /g, " "),
-            (text) => text.replace(/ from /g, " "),
-            (text) => text.replace(/ to /g, " ")
-        ];
+        const unWantedWords = [" the ", " and ", " but ", " then ", " a ", " of ",
+                              " than ", " an ", " in ", " on ", " by ", " he ", " she ",
+                              " had ", " gone ", " that ", " from ", " that ", " from ",
+                              " to ", " who ", " whose "];
+
+        const replacer = (replace, text) => {
+            return text.replace(new RegExp(replace, 'ig'), " ");
+        };
 
         const nGramsCommand = new scribe.api.Command('ngrams');
 
         nGramsCommand.execute = () => {
-            const text = unWords.reduce((val, fn) => {
-                return fn(val);
+            const text = unWantedWords.reduce((current, replace) => {
+                return replacer(replace, current);
             }, scribe.el.innerText);
 
             const bigrams = NGrams.bigrams(text);
+            debugger;
             const ngrams = NGrams.ngrams(text, 1);
 
             return {
