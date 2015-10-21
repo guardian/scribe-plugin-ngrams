85270b6e5f81c1950b223e79452f59303c954b93
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-10-21 09:39:18 +0100

    Updated with suggestsion

--- src/scribe-plugin-ngrams.js
+++ src/scribe-plugin-ngrams.js
@@ -5,13 +5,14 @@ module.exports = function(config) {
     const NGrams = natural.NGrams;
 
     return function(scribe) {
-        const unWantedWords = [" the ", " and ", " but ", " then ", " a ", " of ",
-                              " than ", " an ", " in ", " on ", " by ", " he ", " she ",
-                              " had ", " gone ", " that ", " from ", " that ", " from ",
-                              " to ", " who ", " whose "];
+        const unWantedWords = ["the", "and", "but", "then", "a", "of",
+                              "than", "an ", " in ", " on ", "by", "he", "she",
+                              "had", "gone", "that", "from", "that", "from",
+                              "to", "who", "whose"];
 
         const replacer = (replace, text) => {
-            return text.replace(new RegExp(replace, 'ig'), " ");
+            const str = " " + replace + " ";
+            return text.replace(new RegExp(str, 'ig'), " ");
         };
 
         const nGramsCommand = new scribe.api.Command('ngrams');
@@ -22,7 +23,6 @@ module.exports = function(config) {
             }, scribe.el.innerText);
 
             const bigrams = NGrams.bigrams(text);
-            debugger;
             const ngrams = NGrams.ngrams(text, 1);
 
             return {
